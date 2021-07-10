b556e5035a160f3f1b921835bda28fe1273c303f
ksc999 <ksc19@mails.tsinghua.edu.cn>
2021-07-10 13:52:53 +0800

    学培部SAST TUTOR仓库建立！

---
+++ python/文档查阅/myfile.py
@@ -0,0 +1,15 @@
+"""
+Module documentation
+"""
+
+def square(x):
+    """
+    Function documentation
+    """
+    return x**2
+
+class Student:
+    """
+    class documentation
+    """
+    pass
