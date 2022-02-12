c5a06a21c6412cfd7d1eec83ed63a47cc8450a71
zhengty20 <zhengzong20010904@163.com>
2022-02-12 10:52:54 +0800

    第四讲
    Static and Namespace

---
+++ C++/第四讲/Static and Namespace/codes/多文件namespace/math.cpp
@@ -0,0 +1,10 @@
+#include "add.h"
+#include "subtract.h"
+namespace Math {
+    int add(int x, int y) {
+        return x + y;
+    }
+    int subtract(int x, int y) {
+        return x - y;
+    }
+}
