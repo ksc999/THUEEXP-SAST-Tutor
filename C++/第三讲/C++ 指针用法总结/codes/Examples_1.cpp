fba6403dd215e7fa6d371ae6fa4b91bc4ddd19a5
Yunhao Lan <435245767@qq.com>
2022-02-27 23:56:29 +0800

    第一次更新
    C++指针用法总结部分已经上传

---
+++ C++/第三讲/C++ 指针用法总结/codes/Examples_1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+int main(){
+	int x{ 7 };
+	int y{ 3 };
+	int* ptr{ &x };
+
+	std::cout << &x << std::endl;
+	std::cout << x << std::endl;
+	std::cout << ptr << std::endl;
+	std::cout << *ptr << std::endl;
+
+	*ptr = 9;
+
+	std::cout << &x << std::endl;
+	std::cout << x << std::endl;
+	std::cout << ptr << std::endl;
+	std::cout << *ptr << std::endl;
+
+	ptr = &y;
+
+	std::cout << &y << std::endl;
+	std::cout << y << std::endl;
+	std::cout << ptr << std::endl;
+	std::cout << *ptr << std::endl;
+
+	std::cout << sizeof(ptr) << std::endl;
+	std::cout << sizeof(*ptr) << std::endl;
+
+	return 0;
+}
---
+++ C++/第三讲/C++ 指针用法总结/codes/Examples_2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+void add(int a, int b)
+{
+    std::cout << "加和为 " << a + b;
+}
+void sub(int a, int b)
+{
+    std::cout << "相差为 " << a - b;
+}
+void mul(int a, int b)
+{
+    std::cout << "相乘为" << a * b;
+}
+void fun1() {
+    std::cout << "Fun1";
+}
+void fun2() {
+    std::cout << "Fun2";
+}
+void funx(void (*fun)()){
+    fun();
+}
+
+int main()
+{
+
+    //关于函数指针数组的主程序部分
+
+    /*
+    void (*fun_ptr_arr[])(int, int) = { add, sub, mul };
+    int x, a = 1, b = 2;
+
+    std::cout << "请选择: 输入0让两数相加, 输入1让两数相减，输入2让两数相乘\n";
+    std::cin >> x;
+
+    if (x > 2) 
+        return 0;
+    (*fun_ptr_arr[x])(a, b);
+    return 0;
+    */
+
+    //关于函数指针作为参数传递的主程序部分
+
+    /*
+    funx(fun1);
+    funx(fun2);
+    return 0;
+    */
+}
